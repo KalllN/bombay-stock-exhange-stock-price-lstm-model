5b8fe6b7341decb03d7a68b2e0a9d14b80db1407
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-16 15:27:31 +0530

    using MinMaxScaler

---
+++ scaling data/scaled_data.py
@@ -0,0 +1,21 @@
+#Importing the libraries
+import math
+import pandas_datareader as web
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+#Getting the data
+df = web.DataReader('JPM', data_source = 'yahoo', start = '2012-01-01', end = '2020-12-31')
+
+#creating new datafile with only 'close' column
+data = df.filter(['Close'])
+#Convert the dataframe to a numpy array
+dataset = data.values
+
+#Scaling data
+scaler = MinMaxScaler(feature_range = (0, 1))
+scaled_data = scaler.fit_transform(dataset)
