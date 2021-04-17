8f36bd09310c3f7abe31fb17b3aa962aec97ca65
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-17 15:14:19 +0530

    Update prices_final.py

--- predictions/prices_final.py
+++ predictions/prices_final.py
@@ -1,6 +1,6 @@
-jpm = web.DataReader('JPM', data_source = 'yahoo', start = '2012-01-01', end = '2020-12-17')
+bse = web.DataReader('^BSESN', data_source = 'yahoo', start = '2012-01-01', end = '2021-04-15')
 
-new_df = jpm.filter(['Close'])
+new_df = bse.filter(['Close'])
 
 #getting last 60 day closing price value
 last_60_days = new_df[-60:].values
@@ -17,6 +17,6 @@ X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 pred_price = model.predict(X_test)
 pred_price = scaler.inverse_transform(pred_price)
 
-jpm1 = web.DataReader('JPM', data_source = 'yahoo', start = '2020-12-18', end = '2020-12-18')
-print(jpm1['Close'])
+bse1 = web.DataReader('^BSESN', data_source = 'yahoo', start = '2021-04-16', end = '2021-04-16')
+print(bse1['Close'])
 print(pred_price)
